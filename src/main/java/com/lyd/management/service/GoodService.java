1cc32e69534942deb725517dcb5c3c3f4d80811a
lyd11250 <1125025634@qq.com>
2022-07-07 21:17:17 +0800

    货品管理模块 -- 修复修改货品信息时无法取消的BUG

--- src/main/java/com/lyd/management/service/GoodService.java
+++ src/main/java/com/lyd/management/service/GoodService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface GoodService {
     List<Good> selectAllService();
+    Good selectByIdService(int id);
     boolean deleteByIdService(int id);
     boolean addGoodService(Good good);
     boolean editGoodService(Good good);
